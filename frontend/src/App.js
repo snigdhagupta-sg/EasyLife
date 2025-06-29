7ba29f13bf5d8c6d1c3d02b9b1e5aa0cbc3ee1c9
Vanisha1606 <vanishag1606@gmail.com>
2025-06-29 11:29:54 +0530

    Integrated chatbot frontend in main frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from 'react';
+import Chatbot from "./components/Chatbot.jsx";
 import DocumentExplainer from './pages/DocumentExplainer.js'; // Adjust the path as necessary
 import FileUpload from './pages/FileUpload.jsx';
 
 function App() {
   return (
 <>
+    <Chatbot />
     <DocumentExplainer />
     <FileUpload />
 </>
